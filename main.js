a6c33ee1ae4cbbf48fcde11faf82c537561ad127
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-01-18 13:08:56 -0500

    Created Start button to initalize quiz.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Body
 var QUIZ_SECTIONS = document.querySelectorAll(".quiz-section");
 
-/Start
+//Start
 var START_SECTION = document.getElementById("start");
 var START_BTN = document.getElementById("start-button");
