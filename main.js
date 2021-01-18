e3d3e15daa8ac2bf94ab78071747a15eab2bd16e
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-01-18 12:58:28 -0500

    Created intro explaining game mechanics.

---
+++ main.js
@@ -0,0 +1,6 @@
+// Body
+const QUIZ_SECTIONS = document.querySelectorAll(".quiz-section");
+
+/Start
+const START_SECTION = document.getElementById("start");
+const START_BTN = document.getElementById("start-button");
