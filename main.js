9ad501f338dfe610b1704f21ca581d15e48ebcea
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-01-20 00:57:52 -0500

    Added end of quizJS.

--- main.js
+++ main.js
@@ -13,3 +13,11 @@ const CHOICES = document.getElementById("choices");
 const CHOICE_STATUSES = document.querySelectorAll(".choice-status");
 const CORRECT = document.getElementById("correct");
 const WRONG = document.getElementById("wrong");
+
+// End Quiz 
+const END_SECTION = document.getElementById("end");
+const END_TITLE = document.getElementById("end-title");
+const SCORE = document.getElementById("score");
+const INITIALS_INPUT = document.getElementById("initials");
+const SUBMIT_SCORE = document.getElementById("submit-score");
+const ERROR_MESSAGE = document.getElementById("error-message");
