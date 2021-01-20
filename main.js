487a834e3f2713882655e3e922cbd79e39d92d9d
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-01-19 23:53:57 -0500

    Added JS for quiz questions section status.

--- main.js
+++ main.js
@@ -4,3 +4,12 @@ var QUIZ_SECTIONS = document.querySelectorAll(".quiz-section");
 // Start
 const START_SECTION = document.getElementById("start");
 const START_BTN = document.getElementById("start-button");
+
+// Quiz Questions
+const QUIZ_SECTION = document.getElementById("quiz-questions");
+const TIME_REMAINING = document.getElementById("time-remaining");
+const QUESTION = document.getElementById("question");
+const CHOICES = document.getElementById("choices");
+const CHOICE_STATUSES = document.querySelectorAll(".choice-status");
+const CORRECT = document.getElementById("correct");
+const WRONG = document.getElementById("wrong");
