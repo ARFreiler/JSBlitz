12cb9938ea7a58d735883402d92707098b288105
Ashleigh Freiler <ashleighfreiler@gmail.com>
2021-01-20 22:49:07 -0500

    Created event lister in Highscore JS file.

--- highscore.js
+++ highscore.js
@@ -1,3 +1,6 @@
 // Highscores
 const HIGHSCORE_TABLE = document.getElementById("highscores-table");
 const CLEAR_HIGHSCORE_BTN = document.getElementById("clear-highscores");
+
+// Event listener
+CLEAR_HIGHSCORE_BTN.addEventListener('click', clearHighscores);
